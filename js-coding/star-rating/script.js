48b5e82a03e83e7c2c17fca59a888627f647e3ef
chinna222 <gondigangadhar@gmail.com>
2024-09-21 09:30:47 +0530

    start rating component code organised

--- js-coding/star-rating/script.js
+++ js-coding/star-rating/script.js
@@ -1,49 +1,51 @@
 const stars = document.querySelectorAll("i");
+const starContainer = document.getElementById("star-rating");
+let selectedStarIndex = -1; // Keeps track of the clicked star
 
+// Helper function to set star colors
+function updateStarColors(maxIndex) {
+  stars.forEach((star, ind) => {
+    star.style.color = ind <= maxIndex ? "gold" : "#6b6b6b";
+  });
+}
+
+// Mouseover event handler
 function handleMouseOver(event) {
   const star = event.currentTarget;
   const index = Array.from(stars).indexOf(star);
-  getStarColor(star, index);
+  updateStarColors(index);
 }
 
+// Mouseleave event handler to reset stars if no star is selected
+function handleMouseLeave() {
+  updateStarColors(selectedStarIndex);
+}
+
+// Click event handler
+function handleClick(event) {
+  const star = event.currentTarget;
+  const index = Array.from(stars).indexOf(star);
+  selectedStarIndex = index;
+
+  // Remove mouseover functionality once a star is clicked
+  stars.forEach((star) =>
+    star.removeEventListener("mouseover", handleMouseOver)
+  );
+  starContainer.removeEventListener("mouseleave", handleMouseLeave);
+
+  updateStarColors(index);
+}
+
+// Add event listeners to stars
 function addEventListeners() {
   stars.forEach((star, index) => {
     star.addEventListener("mouseover", handleMouseOver);
-    star.addEventListener("click", () => {
-      onClickStar(star, index);
-    });
+    star.addEventListener("click", handleClick);
   });
+
+  // Add mouseleave listener to container to reset color when leaving
+  starContainer.addEventListener("mouseleave", handleMouseLeave);
 }
 
-const getStarColor = (star, index) => {
-  stars.forEach((starEle, ind) => {
-    if (Number(star.id) > ind) {
-      starEle.style.color = "gold";
-    } else {
-      starEle.style.color = "#6b6b6b";
-    }
-  });
-};
-
-const onMouseleave = () => {
-  stars.forEach((starEle, ind) => {
-    starEle.style.color = "#6b6b6b";
-  });
-};
-
-const starContainer = document.getElementById("star-rating");
-starContainer.addEventListener("mouseleave", onMouseleave);
-
-const onClickStar = (star, index) => {
-  stars.forEach((starEle, ind) => {
-    starEle.removeEventListener("mouseover", handleMouseOver);
-    if (Number(star.id) > ind) {
-      starEle.style.color = "gold";
-    } else {
-      starEle.style.color = "#6b6b6b";
-    }
-  });
-  starContainer.removeEventListener("mouseleave", onMouseleave);
-};
-
+// Initialize
 addEventListeners();
